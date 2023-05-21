16259a7d7a9432f3c9bb33d3cd4daafab924de4d
Pijush Sarker <pijushsarker.dev@gmail.com>
2023-05-21 21:05:07 +0600

    hide user and pass

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
+require('dotenv').config()
 const app = express();
 
 const port = process.env.PORT || 5000;
@@ -11,8 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 
-
-const uri = "mongodb+srv://boomAuto:eskQSAAVgnhuIjUH@cluster0.woapvgk.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.woapvgk.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
